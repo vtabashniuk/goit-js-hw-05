6d91909c1e12633d169fd3ac26ff2fd0dd907a60
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-06-25 06:53:32 +0300

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,22 @@
+/*
+Напиши функцию-конструктор Account, которая создает объект со свойствами login и email. В prototype функции-конструктора добавь метод getInfo(), который выводит в консоль значения полей login и email объекта который его вызвал.
 
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
+*/
 
 
 
