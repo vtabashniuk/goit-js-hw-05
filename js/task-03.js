19c0bc4f0e60491aa795ed3c86603f1547d57928
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-06-25 08:08:45 +0300

    Добавляет решение Задачи - 3

---
+++ js/task-03.js
@@ -0,0 +1,64 @@
+/*
+Напиши класс Storage, который будет создавать объекты для управления складом товаров. При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+Добавь методы класса:
+
+getItems() - возвращает массив текущих товаров
+addItem(item) - получает новый товар и добавляет его к текущим
+removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+*/
+
+class Storage {
+    constructor(items) {
+        this.items = [...items];
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    removeItem(item) {
+        for (const element of this.items) {
+            if (element !== item) continue;
+            this.items.splice(items.indexOf(element), 1);
+        }
+    }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('rererer');
+console.table(storage.items);
